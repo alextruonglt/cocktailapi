38538e5a841ae20836b982ec9326bdf8417a1f5f
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-08-29 14:50:25 -0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    
+      <section className="section about-section">
+          <h1 className="section-title">About us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quia dicta esse eligendi inventore, illo harum ipsum aperiam veritatis laboriosam, excepturi officiis neque, voluptas eum provident praesentium adipisci dignissimos voluptatum?</p>
+      </section>
+  
+  )
+}
+
+export default About
